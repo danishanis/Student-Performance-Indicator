a2d86e4d3ee483b96a588d35d19ceb45ecce723f
Danish Anis <danishanis10@hotmail.com>
2026-01-23 22:18:52 -0500

    customizing exception message

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,9 @@ import sys # controlling exceptions with sys library
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
+    """
+    custom message to be pushed in logs whenever exception occurs
+    """
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
