ecb902dbcffed0a2f4df649941a51576a5bbbb21
Danish Anis <danishanis10@hotmail.com>
2026-01-26 16:45:53 -0500

    Adding informative comments on logging & setting logging configurations

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,20 @@ import logging
 import os
 from datetime import datetime
 
+# how the log file name will be created with datetime stamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# joining the log file name with logs folder in current working directory
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# setting the logging configuration - overwriting the default logging config
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.INFO, # Can be called by logging.info()
 
 
 )
